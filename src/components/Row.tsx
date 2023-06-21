c38fc340c77f055f4ca96cc4f8f70cb96c7abb07
Joebye <evgenii.prs@gmail.com>
2023-06-21 12:59:32 +0300

    life game with next method and size divs

--- src/components/Row.tsx
+++ src/components/Row.tsx
@@ -1,10 +1,22 @@
-import { ReactNode } from "react"
+import { ReactNode, useEffect, useState } from "react"
+import configLife from "../config/life-game-config.json"
+
+function getSize() {
+       return Math.min(window.innerHeight, window.innerWidth) / configLife.dimension - 2;
+    }
 
 const Row: React.FC<{row: number[]}> = ({row}) => {
+     const [size, setSize] = useState(getSize()); 
+    useEffect(() => {
+        window.addEventListener('resize', () => setSize(getSize()) );
     
+    }, []) 
+
+
     function getDivs(): ReactNode {
+       
         return row.map((num, ind) => 
-        <div key={ind} style={{border: "solid 1px grey", width: 10, height: 10, backgroundColor: num? 'black' : 'white'}}></div>)
+        <div key={ind} style={{width: size, height: size, border: "solid 1px grey",  backgroundColor: num? 'black' : 'white'}}></div>)
     }
 
     return <section style={{display: 'flex'}}>
--- src/service/util/number-functions.js
+++ src/service/util/number-functions.js
@@ -2,6 +2,7 @@ export function range(min, max) {
     return Array.from({length: max - min})
     .map((__, index) => index + min)
 }
+
 export function count(array, field, interval) {
     return array.reduce((res, cur)=>{
         const intervalNumber = Math.trunc(cur[field] / interval);
@@ -10,3 +11,13 @@ export function count(array, field, interval) {
          return res;
     }, {});
 }
+
+export function arraySum(array) {
+    return  array.reduce((sum, cur) => sum + cur, 0);
+}
+
+export function matrixSum(matrix) {
+    return matrix.reduce((sum, cur) => sum + arraySum(cur), 0);
+} 
+
+
